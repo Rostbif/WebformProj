9b3765af0773d29c6b78a52506a55e406772c009
Ofir Adany <ofiradany@gmail.com>
2024-09-02 20:47:30 +0300

    added db connection

---
+++ Client/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["datWise"].ConnectionString;
+            DataTable employees = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employees"; // Adjust the query as needed
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(employees);
+                    }
+                }
+            }
+
+            var x = employees;
+
+        }
+    }
+}
--- DatwiseProject/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ DatwiseProject/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d14a471e383642e0f0293ed1fc9761d028bcc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0870916f7b676e19ee2b5a92538f3b10339a357e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
