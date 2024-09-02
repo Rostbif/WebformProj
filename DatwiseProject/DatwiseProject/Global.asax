0870916f7b676e19ee2b5a92538f3b10339a357e
Ofir Adany <ofiradany@gmail.com>
2024-09-02 11:46:19 +0300

    a few changes to the project

--- DatwiseProject/DatwiseProject/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatwiseProject.Global" Language="C#" %>
--- DatwiseProject/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ DatwiseProject/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d320ca4fa2f2c8250e17f318e5bd813d21c2a4cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d14a471e383642e0f0293ed1fc9761d028bcc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
